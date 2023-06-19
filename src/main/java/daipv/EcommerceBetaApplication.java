18bf954d50ae4306c9e75b38b8c2fa718a410b19
mrvudai <vudai16666@gmail.com>
2023-06-19 21:44:04 +0700

    create project

---
+++ src/main/java/daipv/EcommerceBetaApplication.java
@@ -0,0 +1,13 @@
+package daipv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBetaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daipv/EcommerceBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package daipv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
